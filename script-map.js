64e159fa0b2ada5c6fe848efb0fd52856eb35499
Elena-Stoyanova <elenastoyanova742@gmail.com>
2023-11-23 03:21:31 +0200

    last change

---
+++ script-map.js
@@ -0,0 +1,118 @@
+let map;
+let newMarkers = [];
+const sofia = { lat: 42.6977, lng: 23.3219 };
+
+const locations = [
+  {
+    position: {
+      lat: 42.69812,
+      lng: 23.320685,
+    },
+    title: 'Локация 1',
+    filters: ['webDesign', 'frontEnd'],
+  },
+  {
+    position: {
+      lat: 42.692981,
+      lng: 23.327455,
+    },
+    title: 'Локация 2',
+    filters: ['backEnd', 'webDesign'],
+  },
+  {
+    position: {
+      lat: 42.695454,
+      lng: 23.314481,
+    },
+    title: 'Локация 3',
+    filters: ['branding', 'consultations'],
+  },
+  {
+    position: {
+      lat: 42.687771,
+      lng: 23.312513,
+    },
+    title: 'Локация 4',
+    filters: ['backEnd', 'frontEnd'],
+  },
+  {
+    position: {
+      lat: 42.693023,
+      lng: 23.332575,
+    },
+    title: 'Локация 5',
+    filters: ['branding', 'consultations'],
+  },
+  {
+    position: {
+      lat: 42.703275,
+      lng: 23.314272,
+    },
+    title: 'Локация 6',
+    filters: ['webDesign', 'frontEnd'],
+  },
+];
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: sofia,
+    zoom: 14,
+  });
+
+  setMarkers(map);
+}
+
+function setMarkers(map) {
+  const infoWindow = new google.maps.InfoWindow();
+  const image = {
+    url: './ellipse.png',
+    size: new google.maps.Size(30, 32),
+    origin: new google.maps.Point(-12, -8),
+    anchor: new google.maps.Point(0, 0),
+  };
+
+  const shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly',
+  };
+
+  locations.forEach(({ position, title, filters }, i) => {
+    const marker = new google.maps.Marker({
+      position,
+      map,
+      filters,
+      icon: image,
+      shape: shape,
+      title,
+      label: `${i + 1}`,
+      optimized: false,
+    });
+    newMarkers.push(marker);
+    marker.addListener('click', () => {
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
+    });
+  });
+}
+
+const activeFilters = [];
+
+function filterMarkers(category) {
+  if (activeFilters.includes(category)) {
+    const index = activeFilters.indexOf(category);
+    activeFilters.splice(index, 1);
+  } else {
+    activeFilters.push(category);
+  }
+
+  for (i = 0; i < newMarkers.length; i++) {
+    let marker = newMarkers[i];
+    let filters = marker.filters;
+
+    const isVisible = activeFilters.every((f) => filters.includes(f));
+    marker.setVisible(isVisible);
+  }
+}
+
+window.initMap = initMap;
--- script.js
+++ script.js
@@ -1,40 +1,44 @@
 let showSearch = false;
 function showSearchForm() {
   let searchButton = document.querySelector('.search-container');
-  
+  let navBar = document.querySelector('.nav-bar');
   showSearch = !showSearch;
   if (showSearch) {
     searchButton.classList.add('expandet');
-    
+    navBar.classList.add('expandet');
   } else {
     searchButton.classList.remove('expandet');
-    
+    navBar.classList.remove('expandet');
   }
 }
 
 let showMenuItems = false;
 function showMenu() {
   let menu = document.querySelector('.menu-bar');
+  let searchContainer = document.querySelector('.search-container');
+  let lang = document.querySelector('.lang-dropdown');
   showMenuItems = !showMenuItems;
   if (showMenuItems) {
     menu.classList.add('showMenu');
-    
+    searchContainer.classList.add('showMenu');
+    lang.classList.add('showMenu');
   } else {
     menu.classList.remove('showMenu');
-   
+    searchContainer.classList.remove('showMenu');
+    lang.classList.remove('showMenu');
   }
 }
-
 
 let showLanguages = false;
 function showLanguageBar() {
   let lang = document.querySelector('.lang-dropdown');
+  let line = document.querySelector('.header-line'); 
   showLanguages = !showLanguages;
   if (showLanguages) {
     lang.classList.add('showLang');
-    
+    line.classList.add('showLang');
   } else {
     lang.classList.remove('showLang');
-   
+    line.classList.remove('showLang');
   }
 }
